6c7aec7c80b01b637ad20a11170673f016dc627b
Moni Waterhouse <monica.waterhouse@estudiantec.cr>
2022-11-10 00:03:23 -0600

    Enable CORS

--- TiendaAPI/Program.cs
+++ TiendaAPI/Program.cs
@@ -1,11 +1,20 @@
 var builder = WebApplication.CreateBuilder(args);
 
+string _MyCors = "MyCors";
+
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options => {
+    options.AddPolicy(name: _MyCors, builder => {
+        builder.SetIsOriginAllowed(origin => new Uri(origin).Host == "localhost")
+        .AllowAnyHeader().AllowAnyHeader();
+    });
+});
 
 var app = builder.Build();
 
@@ -16,10 +25,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
-
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseCors(_MyCors);
 
 app.Run();
